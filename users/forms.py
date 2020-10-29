c64a473458ace69b3e36866ff058da2ad94b2e01
Buzzlay <dimabuslaev@gmail.com>
2020-10-29 14:38:27 +0500

    update project

---
+++ users/forms.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+from .models import Contact
+from django import forms
+
+
+User = get_user_model()
+
+
+#  создадим собственный класс для формы регистрации
+#  сделаем его наследником предустановленного класса UserCreationForm
+class CreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        # укажем модель, с которой связана создаваемая форма
+        model = User
+        # укажем, какие поля должны быть видны в форме и в каком порядке
+        fields = ("first_name", "last_name", "username", "email")
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        # на основе какой модели создаётся класс формы
+        model = Contact
+        # укажем, какие поля будут в форме
+        fields = ('name', 'email', 'subject', 'body')
+
+    def clean_subject(self):
+        data = self.cleaned_data['subject']
+
+        if "спасибо" not in data.lower():
+            raise forms.ValidationError("Вы обязательно должны нас поблагодарить!")
+        return data
---
+++ users/views.py
@@ -0,0 +1,54 @@
+#  импортируем CreateView, чтобы создать ему наследника
+from django.views.generic import CreateView
+
+#  функция reverse_lazy позволяет получить URL по параметру "name" функции path()
+#  берём, тоже пригодится
+from django.urls import reverse_lazy
+from django.shortcuts import render, redirect
+
+#  импортируем класс формы, чтобы сослаться на неё во view-классе
+from .forms import CreationForm, ContactForm
+
+
+class SignUp(CreateView):
+    form_class = CreationForm
+    success_url = reverse_lazy("login")
+    # где login — это параметр "name" в path()
+    template_name = "signup.html"
+
+
+def user_contact(request):
+    # проверим, пришёл ли к нам POST-запрос или какой-то другой:
+    if request.method == 'POST':
+        # создаём объект формы класса ContactForm и передаём в него полученные данные
+        form = ContactForm(request.POST)
+
+        # если все данные формы валидны - работаем с "очищенными данными" формы
+        if form.is_valid():
+            # берём валидированные данные формы из словаря form.cleaned_data
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['body']
+            # при необходимости обрабатываем данные
+            # ...
+            # сохраняем объект в базу
+            form.save()
+
+            # Функция redirect перенаправляет пользователя
+            # на другую страницу сайта, чтобы защититься
+            # от повторного заполнения формы
+            return redirect('/')
+
+        # если условие if form.is_valid() ложно и данные не прошли валидацию -
+        # передадим полученный объект в шаблон
+        # чтобы показать пользователю информацию об ошибке
+
+        # заодно заполним прошедшими валидацию данными все поля,
+        # чтобы не заставлять пользователя вносить их повторно
+        return render(request, 'contact.html', {'form': form})
+
+    # если пришёл не POST-запрос - создаём и передаём в шаблон пустую форму
+    # пусть пользователь напишет что-нибудь
+    form = ContactForm()
+    return render(request, 'contact.html', {'form': form})
